0aa0d5fad0cd358e7a5b9903c0adbc175d2da8f5
williamhamilton <william.hamilton@gmail.com>
2017-08-29 22:32:47 +1200

    Update list_tickets.sql

--- list_tickets.sql
+++ list_tickets.sql
@@ -1,15 +1,16 @@
-SELECT `ost_ticket`.`number`, `ost_user`.`name`, `ost_ticket__cdata`.`subject`, `ost_staff`.`username`, `ost_ticket_status`.`name`, `ost_ticket`.`closed`, `ost_organization`.`name` 
+SELECT `ost_ticket`.`number` AS `Ticket Number`, `ost_user`.`name` AS `Client Ticket Owner`, `ost_ticket__cdata`.`subject` AS `Subject`,
+ `ost_staff`.`username` AS `Staff Member`, `ost_ticket_status`.`name` AS `Ticket Status`,
+ `ost_ticket`.`closed` AS `Closed Date`, `ost_organization`.`name` AS `Client Org` 
 FROM
  { oj `helpdesk`.`ost_ticket` AS `ost_ticket` 
-	LEFT OUTER JOIN
-  `helpdesk`.`ost_user` AS `ost_user` ON `ost_ticket`.`user_id` = `ost_user`.`id`
-    RIGHT OUTER JOIN 
-  `helpdesk`.`ost_ticket__cdata` AS `ost_ticket__cdata` ON `ost_ticket__cdata`.`ticket_id` = `ost_ticket`.`ticket_id` }, 
-  `helpdesk`.`ost_staff` AS `ost_staff`, `helpdesk`.`ost_ticket_status` AS `ost_ticket_status`,
-  `helpdesk`.`ost_organization` AS `ost_organization` 
+LEFT OUTER JOIN `helpdesk`.`ost_user` AS `ost_user` ON `ost_ticket`.`user_id` = `ost_user`.`id` 
+RIGHT OUTER JOIN `helpdesk`.`ost_ticket__cdata` AS `ost_ticket__cdata` ON `ost_ticket`.`ticket_id` = `ost_ticket__cdata`.`ticket_id` },
+ `helpdesk`.`ost_staff` AS `ost_staff`,
+ `helpdesk`.`ost_ticket_status` AS `ost_ticket_status`,
+ `helpdesk`.`ost_organization` AS `ost_organization` 
 WHERE
  `ost_staff`.`staff_id` = `ost_ticket`.`staff_id` 
-   AND
+AND
  `ost_ticket_status`.`id` = `ost_ticket`.`status_id` 
-   AND
+AND
  `ost_organization`.`id` = `ost_user`.`org_id`
